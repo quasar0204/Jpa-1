59f3133de41605e12341a30455cc0236ddf3ab89
quasar0204 <quasar13245@gmail.com>
2021-01-20 21:51:30 +0900

    210120 - 활용1 완강

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -28,4 +28,11 @@ public class ItemService {
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
+
+    @Transactional
+    public void updateItem(Long id, String name, int price) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+    }
 }
